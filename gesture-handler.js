bb99352a30a45fd8165097f7d3f9d5b34e3bd0c5
tebando <45377127+tebando@users.noreply.github.com>
2024-02-21 22:04:24 +0900

    Update gesture-handler.js

--- gesture-handler.js
+++ gesture-handler.js
@@ -40,22 +40,23 @@ AFRAME.registerComponent("gesture-handler", {
     },
   
   handleRotation: function (event) {
-  if (this.isVisible) {
+ if (this.isVisible) {
     // 新しい回転値を計算
     var newYRotation = this.el.object3D.rotation.y + event.detail.positionChange.x * this.data.rotationFactor;
-    var newXRotation = this.el.object3D.rotation.x + event.detail.positionChange.y * this.data.rotationFactor;
+    var newZRotation = this.el.object3D.rotation.z + event.detail.positionChange.y * this.data.rotationFactor; // Z軸の回転に変更
 
-    // y軸の回転を-π/2からπ/2（-20度から20度）の範囲に制限
+    // Y軸の回転を-π/9からπ/9（-20度から20度）の範囲に制限
     if (newYRotation > Math.PI/9) newYRotation = Math.PI/9;
     else if (newYRotation < -Math.PI/9) newYRotation = -Math.PI/9;
 
-    // x軸の回転を-πからπの範囲に制限
-    if (newXRotation > Math.PI) newXRotation = Math.PI;
-    else if (newXRotation < -Math.PI) newXRotation = -Math.PI;
+    // Z軸の回転を-π/9からπ/9（-20度から20度）の範囲に制限
+    if (newZRotation > Math.PI/9) newZRotation = Math.PI/9;
+    else if (newZRotation < -Math.PI/9) newZRotation = -Math.PI/9;
 
     // 回転を適用
     this.el.object3D.rotation.y = newYRotation;
-    this.el.object3D.rotation.x = newXRotation;
+    this.el.object3D.rotation.z = newZRotation; // Z軸の適用
+}
   }
 }
 ,
