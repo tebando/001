de29e55ad39eca114296f915a69664849a7dcfa8
tebando <45377127+tebando@users.noreply.github.com>
2024-02-22 09:45:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.onload = (e)=>{
 
 	const sound01 = document.getElementById("m001");
 	const btn01 = document.getElementById("mbtn");
-	btn01.addEventListener("click", (e)=>{
+	btn01.addEventListener("mouseup", (e)=>{
 		const sound = sound01.components.sound;
 		if(sound.isPlaying){
 			sound.pauseSound();
